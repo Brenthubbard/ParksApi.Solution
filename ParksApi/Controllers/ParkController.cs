0b5c62a25bc10caa33e1b311ff82949699fcbc81
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-06-11 13:25:47 -0600

    add delete functionality, packages and scaffolding

--- ParksApi/Controllers/ParkController.cs
+++ ParksApi/Controllers/ParkController.cs
@@ -3,6 +3,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ParksApi.Models;
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
 
 namespace ParksApi.Controllers
 {
@@ -43,6 +47,54 @@ namespace ParksApi.Controllers
       }
       return park;
     }
+    // PUT: api/Parks/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
 
+      _db.Entry(park).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
+    }
+    // DELETE: api/Parks/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id)
+    {
+      var park = await _db.Parks.FindAsync(id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
   }
 }
+  
